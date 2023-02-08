241cf6fa2fa56b8eb2a2b54df76f5a17b66bf9be
rangerboy574 <jdeweese574@gmail.com>
2023-02-08 11:51:50 -0600

    improvements

--- lib/screens/add_objective_screen.dart
+++ lib/screens/add_objective_screen.dart
@@ -21,7 +21,7 @@ class _AddObjectiveScreenState extends State<AddObjectiveScreen> {
   final DateFormat _dateFormat = DateFormat('MMM dd, yyyy');
 
   final List<String> _priorities =
-  ['Idea PlaceHolder', 'Future', 'Near Future', 'Now!'];
+  ['Idea / Place-Holder', 'Future', 'Near Future', 'Now!'];
 
   _handledDatePicker() async {
     final DateTime? date = await showDatePicker(
@@ -42,84 +42,112 @@ class _AddObjectiveScreenState extends State<AddObjectiveScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Column(
+        body: GestureDetector(
+          onTap: () => FocusScope.of(context).unfocus(),
+          child: SingleChildScrollView(
+            child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+              child: Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: <Widget>[
-        GestureDetector(
-          onTap: () => Navigator.pop(context),
-          child: Icon(
-            Icons.arrow_back_ios,
-            size: 30.0,
-            color: Theme.of(context).primaryColor,
-          ),
-        ),
-        const SizedBox(height: 20),
-        const Text(
-          'Add Objective',
-          style: TextStyle(
-            color: Colors.black,
-            fontSize: 30.0,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-        const SizedBox(height: 25.0),
-        Form(
-          key: formKey,
-          child: Column(
-            children: <Widget>[
-              Padding(
-                padding: EdgeInsets.symmetric(vertical: 10.0),
-                child: TextFormField(
-                  style: TextStyle(fontSize: 15.0),
-                  decoration: InputDecoration(
-                    labelText: 'Title',
-                    labelStyle: TextStyle(fontSize: 17.0),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10.0),
-                    ),
-                  ),
-                  validator: (input) => input!.trim().isEmpty
-                      ? 'Please Enter an objective'
-                      : null,
-                  onSaved: (input) => _title = input!,
-                  initialValue: _title,
+              GestureDetector(
+                onTap: () => Navigator.pop(context),
+                child: Icon(
+                  Icons.arrow_back_ios,
+                  size: 30.0,
+                  color: Theme.of(context).primaryColor,
                 ),
               ),
-              Padding(
-                padding: EdgeInsets.symmetric(vertical: 10.0),
-                child: TextFormField(
-                  controller: _dateController,
-                  style: TextStyle(fontSize: 15.0),
-                  onTap: _handledDatePicker,
-                  decoration: InputDecoration(
-                    labelText: 'Date',
-                    labelStyle: TextStyle(fontSize: 17),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10.0),
-                    ),
-                  ),
+              const SizedBox(height: 20),
+              const Text(
+                'Add Objective',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 30.0,
+                  fontWeight: FontWeight.bold,
                 ),
               ),
-              Padding(
-                padding: EdgeInsets.symmetric(vertical: 10.0),
-                child: TextFormField(
-                  style: TextStyle(fontSize: 15.0),
-                  decoration: InputDecoration(
-                    labelText: 'Priority',
-                    labelStyle: TextStyle(fontSize: 17.0),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10.0),
+              const SizedBox(height: 25.0),
+              Form(
+                key: formKey,
+                child: Column(
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 10.0),
+                      child: TextFormField(
+                        style: const TextStyle(fontSize: 15.0),
+                        decoration: InputDecoration(
+                          labelText: 'Title',
+                          labelStyle: const TextStyle(fontSize: 17.0),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10.0),
+                          ),
+                        ),
+                        validator: (input) => input!.trim().isEmpty
+                            ? 'Please Enter an objective'
+                            : null,
+                        onSaved: (input) => _title = input!,
+                        initialValue: _title,
+                      ),
                     ),
-                  ),
-                  validator: (input) => _,
-                  onSaved: (input) => _title = input!,
-                  initialValue: _title,
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 10.0),
+                      child: TextFormField(
+                        readOnly: true,
+                        controller: _dateController,
+                        style: const TextStyle(fontSize: 15.0),
+                        onTap: _handledDatePicker,
+                        decoration: InputDecoration(
+                          labelText: 'Date',
+                          labelStyle: const TextStyle(fontSize: 17),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10.0),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 10.0),
+                      child: DropdownButtonFormField(
+                        icon: const Icon(Icons.add_circle_outline),
+                        iconSize: 25.0,
+                        iconEnabledColor: Theme.of(context).primaryColor,
+                        items: _priorities.map((String priority) {
+                          return DropdownMenuItem(
+                          value: priority,
+                              child: Text(
+                              priority,
+                              style: const TextStyle(
+                              color: Colors.black,
+                              fontSize: 15.0,
+                              ),
+                              ),
+                              );
+                        }).toList(),
+                        style: const TextStyle(fontSize: 18.0),
+                        decoration: InputDecoration(
+                          labelText: 'Priority',
+                          labelStyle: const TextStyle(fontSize: 20.0),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10.0),
+                          ),
+                        ),
+                        validator: (input) => _priority == null?
+                             'Please Select a Priority for this Objective.' : null,
+                        onChanged: (value) {
+                          setState(() {
+                            _priority = value!;
+                          });
+                        },
+                      ),
+                    ),
+                  ],
                 ),
               ),
-            ],
-          ),
-        ),
       ],
-    ));
+    ),
+            ),
+          ),
+        ));
   }
 }
